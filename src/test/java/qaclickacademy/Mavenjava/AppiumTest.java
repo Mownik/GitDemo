e4569cec1bd0197bf6a42f696287319bcbd1278b
mownika <bojankimownika@gmail.com>
2022-08-11 16:47:00 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void MobileApp()
+	{
+		System.out.println("MobileApp");
+	}
+	
+	@Test
+	public void UxApp()
+	{
+		System.out.println("Uxapp");
+	}
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+@Test
+public void DeleteTwitter()
+{
+	System.out.println("DeleteTwitter");
+}
+
+@Test
+public void Debug()
+{
+	System.out.println("Debug");
+}
+}
